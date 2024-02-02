7149cebf975f3fa13fd8e78c9d5d681629032c7f
Lianxia Li <48264943+Lianxiali@users.noreply.github.com>
2024-02-02 15:17:08 -0500

    Update writeSampleDict for OpenFOAM.py
    read from data file (exported by Slicer 3d python script) rather than using numpy_stl.

--- writeSampleDict for OpenFOAM.py
+++ writeSampleDict for OpenFOAM.py
@@ -1,20 +1,38 @@
-import numpy
+import numpy as np
+import matplotlib.pyplot as plt
 from stl import mesh
 from stl import stl
 import glob
 import re
 from pathlib import Path
 
+import scienceplots
+plt.style.use(["science"])
+
 # convert from m to mm
 scale = 0.001 
-files = glob.glob('../crossSectionSTL/Section*.stl')
-print(files)
+stl_files = glob.glob('../crossSectionData/STL/Section*.stl')
+
+print(stl_files)
 # sort file name numerically, 1,2,3,...,11,12,...
-files = sorted(files, key=lambda x:float(re.findall("(\d+)",x)[0]))
+stl_files = sorted(stl_files, key=lambda x:float(re.findall("(\d+)",x)[0]))
 
 section_sample_file = open(r"120/system/sampledata.txt", "w")
 section_average_file = open(r"120/system/section_average_dict.txt", "w")
 
+centerline_file = '../crossSectionData/section_data.txt'
+centerline_data = np.loadtxt(centerline_file, skiprows=1)
+
+cx = centerline_data[:,1]*scale
+cy = centerline_data[:,2]*scale
+cz = centerline_data[:,3]*scale
+nx = centerline_data[:,4]
+ny = centerline_data[:,5]
+nz = centerline_data[:,6]
+area = centerline_data[:,7] 
+diameter = centerline_data[:,8] 
+
+d = np.zeros(len(cx))  
 str = """
     type            surfaceFieldValue;
     libs            (fieldFunctionObjects);
@@ -38,23 +56,31 @@ str = """
 """
 
 
-for file in files:
+for i, file in enumerate(stl_files):
     print(file)
-    your_mesh = mesh.Mesh.from_file(file)
-    unit_normals = your_mesh.get_unit_normals()
+    # your_mesh = mesh.Mesh.from_file(file)
+    # unit_normals = your_mesh.get_unit_normals()
     # The normal calculated by this tool has small numbers, we use the orginal normal vector 
-    your_mesh.normals = unit_normals
+    # your_mesh.normals = unit_normals
     # convert to ASCII format, note that the STL is not scaled.
     fname = Path(file).stem
     # In slicer 3d, when saving stl, do not select compress, then it will be in ASCII
     # your_mesh.save(file, mode=stl.ASCII, update_normals =False)
 
+    if (i==0):
+        d[i]=0
+    else:
+        dx = cx[i]-cx[i-1]
+        dy = cy[i]-cy[i-1]
+        dz = cz[i]-cz[i-1]
+        d[i] = d[i-1] + np.sqrt(dx*dx + dy*dy + dz*dz)
+ 
     # dump sampleDict for OpenFOAM
     section_sample_file.write(f"{fname}\n")
     section_sample_file.write("{\n")
     section_sample_file.write("\ttype\tplane;\n")
-    section_sample_file.write(f"\tpoint\t({your_mesh.v0[0][0]*scale}\t{your_mesh.v0[0][1]*scale}\t{your_mesh.v0[0][2]*scale});\n")
-    section_sample_file.write(f"\tnormal\t({unit_normals[0][0]}\t{unit_normals[0][1]}\t{unit_normals[0][2]});\n")
+    section_sample_file.write(f"\tpoint\t({cx[i]}\t{cy[i]}\t{cz[i]});\n")
+    section_sample_file.write(f"\tnormal\t({nx[i]}\t{ny[i]}\t{nz[i]});\n")
     section_sample_file.write("\tinterpolate\ttrue;\n")
     section_sample_file.write("}\n")
 
@@ -70,8 +96,8 @@ for file in files:
     section_average_file.write("\tsampledSurfaceDict\n")
     section_average_file.write("\t{\n")
     section_average_file.write("\t\ttype\tplane;\n")
-    section_average_file.write(f"\t\tpoint\t({your_mesh.v0[0][0]*scale}\t{your_mesh.v0[0][1]*scale}\t{your_mesh.v0[0][2]*scale});\n")
-    section_average_file.write(f"\t\tnormal\t({unit_normals[0][0]}\t{unit_normals[0][1]}\t{unit_normals[0][2]});\n")
+    section_average_file.write(f"\t\tpoint\t({cx[i]}\t{cy[i]}\t{cz[i]});\n")
+    section_average_file.write(f"\t\tnormal\t({nz[i]}\t{ny[i]}\t{nz[i]});\n")
     section_average_file.write("\t\tinterpolate\ttrue;\n")
     section_average_file.write("\t}\n")
     section_average_file.write("}\n")
@@ -82,5 +108,24 @@ for file in files:
 section_sample_file.close()
 section_average_file.close()
 
+
+print("itme\tmin\tmax\taverage")
+print(f"diameter\t{np.min(diameter)}\t{np.max(diameter)}\t{np.average(diameter)}\t")
+print(f"area\t{np.min(area)}\t{np.max(area)}\t{np.average(area)}\t")
+
+fig, ax1 = plt.subplots()
+ax2 = ax1.twinx()
+d = d/scale # m to mm
+d = d*(-1) + (d[-1] + d[0]) # transform from [0, 1] to [1, 0]
+ax1.plot(d, diameter, "r-", label = "Diameter")
+ax2.plot(d, area, 'b--', label = "Area")
+
+ax1.set_xlabel(r"Distance (mm)")
+ax1.set_ylabel(r"Diamater (mm)")
+ax2.set_ylabel(r"Area (mm$^2$)")
+plt.tight_layout()
+fig.legend(bbox_to_anchor=(0.6,0.95), ncol=1) # Note: fig, not plt, otherwise only show the last one
+# fig.legend(loc='best') # Automatic legend placement (loc='best') not implemented for figure legend
+plt.show()
 
 
